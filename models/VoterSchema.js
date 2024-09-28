149c1c784febb84cb2229b562096d97dcb5d7fab
kshitijofficial <srivastavakshitijprofessional@gmail.com>
2024-09-28 12:40:27 +0530

    static images

--- models/VoterSchema.js
+++ models/VoterSchema.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose')
+
 const VoterSchema= new mongoose.Schema({
     accountAddress:{
         type:String,
--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -6,15 +6,13 @@ const multer = require("../middlewares/multer")
 
 router.post('/postCandidateImage',authentication,multer.uploadCandidate,async(req,res)=>{
     try{
-        console.log("postCandidate")
-        // const {accountAddress,imageName}=req.body;
-
-        // const saveCandidate = await CandidateModel.create({
-        //     accountAddress:accountAddress,
-        //     imageName:imageName
-        // })
-        // console.log(saveCandidate)
-        // res.status(200).json(saveCandidate)
+        const {accountAddress}=req;
+        const imageName = req.file.filename;
+        await CandidateModel.create({
+            accountAddress,
+            imageName
+        })
+        res.status(200).json({message:"successful"})
 
     }catch(error){
         console.log(error)
