000fdab3111c9475556e71dab4d07df4eaf384dd
shwetap44 <paranjape.shweta1997@gmail.com>
2024-09-30 17:14:00 +0530

    added a route to store voter images

--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -3,12 +3,28 @@ const router = express.Router()
 const {authentication} = require("../middlewares/authentication")
 const CandidateModel = require("../models/CandidateSchema")
 const multer = require("../middlewares/multer")
+const VoterModel = require('../models/VoterSchema')
 
 router.post('/postCandidateImage',authentication,multer.uploadCandidate,async(req,res)=>{
     try{
         const {accountAddress}=req;
         const imageName = req.file.filename;
         await CandidateModel.create({
+            accountAddress,
+            imageName
+        })
+        res.status(200).json({message:"successful"})
+
+    }catch(error){
+        console.log(error)
+    }
+})
+
+router.post('/postVoterImage',authentication,multer.uploadVoter,async(req,res)=>{
+    try{
+        const {accountAddress}=req;
+        const imageName = req.file.filename;
+        await VoterModel.create({
             accountAddress,
             imageName
         })
